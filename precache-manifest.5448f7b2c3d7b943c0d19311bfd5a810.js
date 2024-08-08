d06d4489666f6a157aa5c7a874df55001dc4a687
KashishK58 <kashishk0508@gmail.com>
2024-08-08 16:49:52 +0530

    Updates

---
+++ precache-manifest.5448f7b2c3d7b943c0d19311bfd5a810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2c63d1ed8b22360f6e7721659080ce",
+    "url": "/weatherBoard/index.html"
+  },
+  {
+    "revision": "4a673fa179f8e542dd1d",
+    "url": "/weatherBoard/static/css/main.08a79bd1.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/weatherBoard/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/weatherBoard/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "4a673fa179f8e542dd1d",
+    "url": "/weatherBoard/static/js/main.7670014e.chunk.js"
+  },
+  {
+    "revision": "398cf7d8107c6f3ab0f9",
+    "url": "/weatherBoard/static/js/runtime~main.e977f212.js"
+  }
+]);
